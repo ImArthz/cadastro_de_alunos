894551bd25631bda98df15d6c92b87980800110c
Arthur Mendonça <135072001+ImArthz@users.noreply.github.com>
2023-09-27 19:49:42 -0300

    Add files via upload

--- classe.py
+++ classe.py
@@ -24,6 +24,9 @@ class Aluno(Pessoa):
         self.__matricula = matricula
         self.informacoes = []
 
+    def Consultar_pessoa(self):
+        return f'Nome: {self.getNome()}, Idade: {self.getIdade()}'
+
     def Consultar_aluno(self):
         return f'{super().Consultar_pessoa()}, Matrícula: {self.getMatricula()}'
 
@@ -35,10 +38,11 @@ class Aluno(Pessoa):
 
     def imprimir_todas_informacoes(self):
         print("-----------------------------------------------")
-        print(f"Informações de {self.getNome()}:")
+        print(f"Nome : \t\t{self.getNome()} \nMatrícula: \t\t{self.getMatricula()} \nIdade: \t\t{self.getIdade()} ")
         print("-----------------------------------------------")
         for info in self.informacoes:
             print(info)
+
 
 class Gerenciadora:
     def __init__(self, alunos):
@@ -71,14 +75,14 @@ class Gerenciadora:
                     random.randint(1, 9)) + str(random.randint(1, 9)) + str(random.randint(1, 9))
 
                 if not any(aluno.getMatricula() == temp_aluno_matricula for aluno in alunos):
-                    print(f"matricula gerada : {temp_aluno_matricula}")
+                    print(f"Matrícula gerada: {temp_aluno_matricula}")
                     break
 
             novo_aluno = Aluno(temp_aluno_nome, temp_aluno_idade, temp_aluno_matricula)
             alunos.append(novo_aluno)
-            return "Aluno cadastrado com sucesso!"
+            print("Aluno cadastrado com sucesso!")
         except Exception as error:
-            return f"Erro ao cadastrar aluno: {str(error)}"
+            print(f"Erro ao cadastrar aluno: {str(error)}")
 
     @staticmethod
     def Consultar_aluno(matricula, alunos):
@@ -91,14 +95,11 @@ class Gerenciadora:
                     break  # Saia do loop após encontrar o aluno
             if not encontrado:
                 print("Não existe nenhum aluno com este número de matrícula.")
-                while True:
-                    escolha = input("Deseja visualizar os alunos matriculados?\n\tSim\n\tNão: ").lower()
-                    Gerenciadora.Listar_alunos(alunos) if escolha in ('sim', 's') else (
-                        print("Voltando ao menu...") if escolha in ('não', 'n') else print("Escolha inválida."))
-                    if escolha in ('sim', 's', 'não', 'n'):
-                        break
+                return False  # Nenhum aluno encontrado
+            return True  # Aluno encontrado
         except Exception as error:
-            return f"Erro ao consultar aluno : {str(error)}"
+            print(f"Erro ao consultar aluno: {str(error)}")
+            return False  # Ocorreu um erro durante a consulta
 
     @staticmethod
     def Atualizar_aluno(matricula, alunos):
@@ -107,7 +108,6 @@ class Gerenciadora:
             for aluno in alunos:
                 if aluno.getMatricula() == matricula:
                     temp_aluno_matricula = aluno.getMatricula()
-                    # Delete aluno criar metodo
                     while True:
                         temp_aluno_nome = input("Digite o nome atualizado do aluno: ")
                         if not any(aluno.getNome() == temp_aluno_nome for aluno in alunos):
@@ -122,20 +122,21 @@ class Gerenciadora:
                     aluno_atualizado = Aluno(temp_aluno_nome, temp_aluno_idade, temp_aluno_matricula)
                     alunos.remove(aluno)
                     alunos.append(aluno_atualizado)
-                    return "Aluno atualizado com sucesso!"
+                    print("Aluno atualizado com sucesso!")
                     encontrado = True
                     break
             if not encontrado:
                 print("Não existe nenhum aluno com este número de matrícula.")
                 while True:
                     escolha = input("Deseja visualizar os alunos matriculados?\n\tSim\n\tNão: ").lower()
-                    Gerenciadora.Listar_alunos(alunos) if escolha in ('sim', 's') else (
-                        print("Voltando ao menu...") if escolha in ('não', 'n') else print("Escolha inválida."))
-                    if escolha in ('sim', 's', 'não', 'n'):
+                    if escolha in ('sim', 's'):
+                        Gerenciadora.Listar_alunos(alunos)
+                    elif escolha in ('não', 'n'):
                         break
-
+                    else:
+                        print("Escolha inválida.")
         except Exception as error:
-            return f"Erro ao atualizar aluno : {str(error)}"
+            print(f"Erro ao atualizar aluno: {str(error)}")
 
     @staticmethod
     def Excluir_aluno(matricula, alunos):
@@ -145,11 +146,10 @@ class Gerenciadora:
                 if aluno.getMatricula() == matricula:
                     alunos.remove(aluno)
                     encontrado = True
+                    print("Aluno excluído com sucesso!")
                     break  # Saia do loop após encontrar e excluir o aluno
 
-            if encontrado:
-                return "Aluno excluído com sucesso!"
-            else:
+            if not encontrado:
                 print("Não existe nenhum aluno com esta matrícula.")
                 while True:
                     escolha = input("Deseja visualizar os alunos matriculados?\n\tSim (S)\n\tNão (N): ").lower()
@@ -161,79 +161,83 @@ class Gerenciadora:
                         print("Escolha inválida. Por favor, digite 'S' ou 'N'.")
 
         except Exception as error:
-            return f"Erro ao excluir aluno: {str(error)}"
+            print(f"Erro ao excluir aluno: {str(error)}")
 
     @staticmethod
     def Menu(alunos):
         while True:
-            if __name__ == "__main__":
-                alunos = []  # Lista para armazenar os objetos Aluno
+            print("---------------------------------------------------------")
+            print("\t\t MENU ")
+            print("---------------------------------------------------------")
+            print("1- Criar Aluno")
+            print("2- Buscar Aluno")
+            print("3- Atualizar Aluno")
+            print("4- Excluir Aluno")
+            print("5 - Inserir informações  ")
+            print("6- Imprimir todos os alunos")
+            print("7- Sair")
 
+            try:
+                escolha = int(input("Digite o número da opção desejada: "))
+                if escolha not in (1, 2, 3, 4, 5,6,7):
+                    raise ValueError("Opção inválida")
+            except ValueError as e:
+                print(f"Erro: {e}")
+                continue  # Volte ao início do loop
+
+            if escolha == 1:
+                Gerenciadora.Criar_aluno(alunos)
+            elif escolha == 2:
                 while True:
-                    print("---------------------------------------------------------")
-                    print("\t\t MENU ")
-                    print("---------------------------------------------------------")
-                    print("1- Criar Aluno")
-                    print("2- Buscar Aluno")
-                    print("3- Atualizar Aluno")
-                    print("4- Excluir Aluno")
-                    print("5- Sair")
-
-                    try:
-                        escolha = int(input("Digite o número da opção desejada: "))
-                        if escolha not in (1, 2, 3, 4, 5):
-                            raise ValueError("Opção inválida")
-                    except ValueError:
-                        print("Opção inválida. Por favor, digite um número de opção válido (1, 2, 3, 4 ou 5).")
+                    temp_matricula = input("Digite a matrícula que deseja consultar: ")
+                    aux = input(f"Você digitou {temp_matricula}. Está correto? (S/N) ").lower()
+                    if aux in ('nao', 'n', 'não'):
                         continue  # Volte ao início do loop
+                    elif aux in ('sim', 's'):
+                        encontrado = Gerenciadora.Consultar_aluno(temp_matricula, alunos)
+                        if encontrado:
+                            break  # Saia do loop após consultar o aluno
+                        else:
+                            print("Aluno não encontrado.")
+                            break  # Saia do loop também se o aluno não for encontrado
+                    else:
+                        print("Opção inválida. Por favor, digite 'S' ou 'N'.")
+            elif escolha == 3:
+                while True:
+                    temp_matricula1 = input("Digite a matrícula que deseja atualizar: ")
+                    aux = input(f"Você digitou {temp_matricula1}. Está correto? (S/N) ").lower()
+                    if aux in ('nao', 'n', 'não'):
+                        continue  # Volte ao início do loop
+                    elif aux in ('sim', 's'):
+                        Gerenciadora.Atualizar_aluno(temp_matricula1, alunos)
+                        break  # Saia do loop após consultar o aluno
+                    else:
+                        print("Opção inválida. Por favor, digite 'S' ou 'N'.")
+            elif escolha == 4:
+                while True:
+                    temp_matricula2 = input("Digite a matrícula que deseja excluir: ")
+                    aux = input(f"Você digitou {temp_matricula2}. Está correto? (S/N) ").lower()
+                    if aux in ('nao', 'n', 'não'):
+                        continue  # Volte ao início do loop
+                    elif aux in ('sim', 's'):
+                        escolha_aux = input(f"Você está prestes a excluir o aluno de matrícula {temp_matricula2}. Tem certeza? (S/N)").lower()
+                        if escolha_aux in ('nao', 'n', 'não'):
+                            continue
+                        elif escolha_aux in ('sim', 's'):
+                            Gerenciadora.Excluir_aluno(temp_matricula2, alunos)
+                            break  # Saia do loop após consultar o aluno
+                        else:
+                            print("Opção inválida. Por favor, digite 'S' ou 'N'.")
+                    else:
+                        print("Opção inválida. Por favor, digite 'S' ou 'N'.")
+            elif escolha == 5:
+                pass
+            elif escolha == 6:
+                pass
+            elif escolha == 7:
+                print("Saindo do programa...")
+                break  # Saia do loop principal e encerre o programa
 
-                    if escolha == 1:
-                        Gerenciadora.Criar_aluno(alunos)
-                    elif escolha == 2:
-                        while True:
-                            temp_matricula = input("Digite a matrícula que deseja consultar: ")
-                            aux = input(f"Você digitou {temp_matricula}. Está correto? (S/N) ").lower()
-                            if aux in ('nao', 'n', 'não'):
-                                continue  # Volte ao início do loop
-                            elif aux in ('sim', 's'):
-                                Gerenciadora.Consultar_aluno(temp_matricula, alunos)
-                                break  # Saia do loop após consultar o aluno
-                            else:
-                                print("Opção inválida. Por favor, digite 'S' ou 'N'.")
-                    elif escolha == 3:
-                        while True:
-                            temp_matricula1 = input("Digite a matrícula que deseja atualizar: ")
-                            aux = input(f"Você digitou {temp_matricula1}. Está correto? (S/N) ").lower()
-                            if aux in ('nao', 'n', 'não'):
-                                continue  # Volte ao início do loop
-                            elif aux in ('sim', 's'):
-                                Gerenciadora.Atualizar_aluno(temp_matricula1, alunos)
-                                break  # Saia do loop após consultar o aluno
-                            else:
-                                print("Opção inválida. Por favor, digite 'S' ou 'N'.")
-                    elif escolha == 4:
-                        while True:
-                            temp_matricula2 = input("Digite a matrícula que deseja excluir: ")
-                            aux = input(f"Você digitou {temp_matricula2}. Está correto? (S/N) ").lower()
-                            if aux in ('nao', 'n', 'não'):
-                                continue  # Volte ao início do loop
-                            elif aux in ('sim', 's'):
-                                escolha_aux = input(f"Você está prestes a excluir o aluno de matrícula {temp_matricula2} tem certeza ? (S/N)").lower()
-                                if escolha_aux in ('nao', 'n', 'não'):
-                                    continue
-                                elif escolha_aux in ('sim', 's'):
-                                    Gerenciadora.Excluir_aluno(temp_matricula2, alunos)
-                                    break  # Saia do loop após consultar o aluno
-                                else:
-                                    print("Opção inválida. Por favor, digite 'S' ou 'N'.")
-                            else:
-                                print("Opção inválida. Por favor, digite 'S' ou 'N'.")
-                    elif escolha == 5:
-                        print("Saindo do programa...")
-                        break  # Saia do loop principal e encerre o programa
-
-
-alunos = []  # Lista para armazenar os objetos Aluno
-Gerenciadora.Menu(alunos)
-    
-
+if __name__ == "__main__":
+    alunos = []  # Lista para armazenar os objetos Aluno
+    Gerenciadora.Menu(alunos)
